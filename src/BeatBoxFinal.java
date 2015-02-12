b9068fc6829148a80c80fac15493101fc6042c1e
ZhenyingZhu <zz2283@columbia.edu>
2015-02-11 23:02:10 -0800

    add three functions

---
+++ src/BeatBoxFinal.java
@@ -0,0 +1,127 @@
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import javax.sound.midi.*;
+import javax.swing.*;
+
+public class BeatBoxFinal {
+
+	JFrame theFrame;
+	JPanel mainPanel;
+	JList incomingList;
+	JTextField userMessage;
+	ArrayList<JCheckBox> checkboxList;
+	int nextNum;
+	Vector<String> listVector = new Vector<String>();
+	String userName;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	HashMap<String, boolean[]> otherSeqsMap = new HashMap<String, boolean[]>();
+
+	Sequencer sequencer;
+	Sequence sequence;
+	Sequence mySequence = null;
+	Track track;
+
+	String[] instrumentNames = { "Bass Drum", "Closed Hi-Hat", "Open Hi-Hat",
+			"Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom",
+			"Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell",
+			"Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga" };
+
+	int[] instruments = { 35, 52, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58,
+			47, 67, 63 };
+
+	public static void main(String[] args) {
+		new BeatBoxFinal().startUp(args[0]);
+	}
+
+	public void startUp(String name) {
+		userName = name; 
+		// Open connection
+		try {
+			Socket sock = new Socket("127.0.0.1", 4242); 
+			out = new ObjectOutputStream(sock.getOutputStream()); 
+			in = new ObjectInputStream(sock.getInputStream()); 
+			Thread remote = new Thread(new RemoteReader()); 
+			remote.start(); 
+		} catch(Exception ex) {
+			System.out.println("Couldn't connect"); 
+		}
+		
+		setUpMidi(); 
+		buildGUI(); 
+	}
+	
+	public void buildGUI() {
+		theFrame = new JFrame("Cyber BeatBox"); 
+		BorderLayout layout = new BorderLayout();
+		JPanel background = new JPanel(layout); 
+		background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		checkboxList = new ArrayList<JCheckBox>(); 
+		
+		Box buttonBox = new Box(BoxLayout.Y_AXIS); 
+		JButton start = new JButton("Start"); 
+		start.addActionListener(new MyStartListener());; 
+		buttonBox.add(start); 
+		
+		JButton stop = new JButton("Stop"); 
+		stop.addActionListener(new MyStopListener()); 
+		buttonBox.add(stop); 
+		
+		JButton upTempo = new JButton("Tempo Up"); 
+		upTempo.addActionListener(new MyUpTempoListener());
+		buttonBox.add(upTempo); 
+		
+		JButton downTempo = new JButton("Tempo Down"); 
+		downTempo.addActionListener(new MyDownTempoListener());
+		buttonBox.add(downTempo); 
+		
+		JButton sendIt = new JButton("SendIt"); 
+		sendIt.addActionListener(new MySendListener());
+		buttonBox.add(sendIt); 
+		
+		userMessage = new JTextField(); 
+		buttonBox.add(userMessage); 
+		
+		incomingList = new JList(); 
+		incomingList.addListSelectionListener(new MyListSelectionListener());
+		incomingList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane theList = new JScrollPane(incomingList); 
+		buttonBox.add(theList); 
+		incomingList.setListData(listVector);
+		
+		Box nameBox = new Box(BoxLayout.Y_AXIS); 
+		for (int i = 0; i < 16; i++) { // Magic number not good
+			nameBox.add(new Label(instrumentNames[i])); 
+		}
+		
+		background.add(BorderLayout.EAST, buttonBox); 
+		background.add(BorderLayout.WEST, nameBox); 
+		
+		theFrame.getContentPane().add(background); 
+		GridLayout grid = new GridLayout(16, 16); 
+		grid.setVgap(1);  
+		grid.setHgap(2); 
+		mainPanel = new JPanel(grid); 
+		background.add(BorderLayout.CENTER, mainPanel); 
+		
+		for (int i = 0; i < 256; i++) {
+			JCheckBox c = new JCheckBox(); 
+			c.setSelected(false);
+			checkboxList.add(c); 
+			mainPanel.add(c); 
+		}
+		
+		theFrame.setBounds(50, 50, 300, 300); 
+		theFrame.pack(); 
+		theFrame.setVisible(true); 
+	}
+	
+	public void setUpMidi() {
+		
+	}
+}
